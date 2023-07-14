85734e2fd09b4b3875a1a1d798136285da2ca05a
Adriel Ryan <adrielryan.tj@hotmail.com>
2023-07-13 21:53:49 -0300

    project created

---
+++ src/main/java/com/devsuperior/deslist/DeslistApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.deslist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeslistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeslistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devsuperior/deslist/DeslistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.deslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeslistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
