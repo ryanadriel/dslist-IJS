2ea34d3f2a28c30c9111824b85854425f60bab2d
Adriel Ryan <adrielryan.tj@hotmail.com>
2023-07-17 21:06:32 -0300

    Game seed

--- src/main/java/com/devsuperior/deslist/entities/Game.java
+++ src/main/java/com/devsuperior/deslist/entities/Game.java
@@ -21,25 +21,30 @@ public class Game {
 	@Column(name = "game_year")
 	private Integer year;
 	private String genre;
-	private String platform;
+	private String platforms;
+	private Double score;
 	private String imgUrl;
+	
+	@Column(columnDefinition = "TEXT")
 	private String shortDescription;
-	private String longDescriptino;
+	
+	@Column(columnDefinition = "TEXT")
+	private String longDescription;
 	
 	public Game() {
-		
 	}
 
-	public Game(Long id, String title, Integer year, String genre, String platform, String imgUrl,
-			String shortDescription, String longDescriptino) {
+	public Game(Long id, String title, Integer year, String genre, String platforms, Double score,String imgUrl,
+			String shortDescription, String longDescription) {
 		this.id = id;
 		this.title = title;
 		this.year = year;
 		this.genre = genre;
-		this.platform = platform;
+		this.platforms = platforms;
+		this.score = score;
 		this.imgUrl = imgUrl;
 		this.shortDescription = shortDescription;
-		this.longDescriptino = longDescriptino;
+		this.longDescription = longDescription;
 	}
 
 	public Long getId() {
@@ -74,12 +79,20 @@ public class Game {
 		this.genre = genre;
 	}
 
-	public String getPlatform() {
-		return platform;
+	public String getPlatforms() {
+		return platforms;
 	}
 
-	public void setPlatform(String platform) {
-		this.platform = platform;
+	public void setPlatforms(String platforms) {
+		this.platforms = platforms;
+	}
+
+	public Double getScore() {
+		return score;
+	}
+
+	public void setScore(Double score) {
+		this.score = score;
 	}
 
 	public String getImgUrl() {
@@ -98,12 +111,12 @@ public class Game {
 		this.shortDescription = shortDescription;
 	}
 
-	public String getLongDescriptino() {
-		return longDescriptino;
+	public String getLongDescription() {
+		return longDescription;
 	}
 
-	public void setLongDescriptino(String longDescriptino) {
-		this.longDescriptino = longDescriptino;
+	public void setLongDescriptino(String longDescription) {
+		this.longDescription = longDescription;
 	}
 
 	@Override
